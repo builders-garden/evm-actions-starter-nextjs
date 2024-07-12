a6cc48c3f8191b0d8f4038255524fbb918d07f74
francescocirulli <63744530+francescocirulli@users.noreply.github.com>
2024-07-13 00:36:20 +0200

    init action safe

--- app/api/action/route.ts
+++ app/api/action/route.ts
@@ -4,34 +4,17 @@ import { NextRequest, NextResponse } from "next/server";
 export const GET = async (req: NextRequest) => {
   const evmActionMetadata: EVMAction = {
     title: "Sample EVM Action",
-    description: "This is a sample EVM Action",
+    description: "This is a sample EVM Action Safe Proposal that you can sign through a shortcut",
     image: "https://placehold.co/955x500",
     links: [
-      {
-        targetUrl: `${appURL()}/api/tx`,
-        postUrl: `${appURL()}/tx-success`, // this will be a GET HTTP call
-        label: "Tx",
-        type: ActionLinkType.TX,
-      },
       {
         targetUrl: `${appURL()}/api/signature`,
         postUrl: `${appURL()}/api/signature/success`, // this will be a POST HTTP call
         label: "Signature",
         type: ActionLinkType.SIGNATURE,
       },
-      {
-        targetUrl: `${appURL()}/api/one-click-login`,
-        loginUrl: appURL(), // this will be a GET HTTP call
-        label: "1-click login",
-        type: ActionLinkType.ONE_CLICK_LOGIN,
-      },
-      {
-        targetUrl: `https://builders.garden`,
-        label: "Link",
-        type: ActionLinkType.LINK,
-      },
     ],
-    label: "Sample Button",
+    label: "Sign Proposal",
   };
   return NextResponse.json(evmActionMetadata);
 };
