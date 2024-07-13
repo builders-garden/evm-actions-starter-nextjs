d5dbef543c618f4bbe2380f21bce1ded933ffb8e
lim0n3 <simonestaffa96@gmail.com>
2024-07-13 15:51:06 +0200

    update signature success endpoint

--- app/api/signature/success/route.ts
+++ app/api/signature/success/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
   const body = await req.json();
-  const { signature } = body;
+  const { signature, message, address } = body;
   // do something with the signature
   return NextResponse.json({ message: "ok" });
 };
